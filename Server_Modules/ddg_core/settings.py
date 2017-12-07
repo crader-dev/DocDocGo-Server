1ef243bb6aa1f1c470b5cce4658e0a419134330f
crader-dev <crader@vt.edu>
2017-12-07 15:15:36 -0500

    Added CORS headers to project.

--- Server_Modules/ddg_core/settings.py
+++ Server_Modules/ddg_core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'ddg_api.apps.DdgApiConfig',
@@ -50,6 +51,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
