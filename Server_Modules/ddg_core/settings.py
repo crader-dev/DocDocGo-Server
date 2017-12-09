7701f4bc80e31bad8bc87ddadd55e26dbe3cd5ba
crader-dev <crader@vt.edu>
2017-12-08 19:11:44 -0500

    Updated nginx config to serve Operator Web App (https://github.com/crader-dev/DocDocGo-Operator-Web-App). Changed api static files url to /api/static/.

--- Server_Modules/ddg_core/settings.py
+++ Server_Modules/ddg_core/settings.py
@@ -129,4 +129,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = '/home/ec2-user/DocDocGo-Server/static'
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
